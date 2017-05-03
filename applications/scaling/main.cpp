3b4233055d408c5737031e5b7e4d08554f51551d
Anna Sinelnikova <anna.sinelnikova@physics.uu.se>
2017-05-03 16:38:26 +0200

    new project

---
+++ applications/scaling/main.cpp
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "Vector.h"
+#include "Utilities.h"
+#include "File.h"
+#include "Polymer.h"
+#include "PolymerObservable.h"
+#include "PolymerScaling.h"
+#include "DataHandler.h"
+#include "Logo.h"
+
+using namespace PCA;
+using namespace std;
+
+int main(int np, char **p)
+{	
+    Logo::print();
+
+    if(p[1]==NULL){
+	printf("------------------------\n");
+	printf("Give me the name of the chain as the argument.\n");
+	printf("Then I will open 'data/xyz_<name>.dat'.\n");
+	printf("For example: ./pca 5dn7\n");
+	printf("------------------------\n");
+	printf("If you have more than one chain (which should be separated ");
+	printf("with one ore more empty lines) in that file, I will take all of them ");
+	printf("for the statistics. All chains should have same number of sites in this case.\n");
+	printf("------------------------\n");
+	printf("\n");
+	exit(1);
+    }
+    
+    printf("------------------------\n");
+    printf("I will perform rescaling procedure.\n");
+    printf("------------------------\n");
+    
+    printf("Polymer name: %s\n", p[1]);
+    // create the full name of the input file
+    char str [100];
+    sprintf(str,"xyz_%s.dat",p[1]); //name of input file
+    sprintf(str,"%s",DataHandler::fullName(str).c_str());
+    
+    // name of output file with configurations
+    char confFile[100];
+    sprintf(confFile,"results/%s_configurations.dat",p[1]);
+    
+    // name of output file with number of monomers during scaling procedure
+    char numMonomersFile[100];
+    sprintf(numMonomersFile,"results/%s_numMonomers.dat",p[1]);
+    
+    // name od output file with scaling parameter VS step
+    char* resultFile;
+    resultFile = new char [100];
+    sprintf(resultFile,"results/%s_scalingParamVSsteps.dat",p[1]); 
+    
+    // finding optimal scaling parameter
+    Polymer polymer(str,0,1);
+    double tmp;
+    tmp = PolymerScaling::findCriticalScalingParam(polymer.getNumMonomers());
+    PolymerScaling::ScalingParam sp(tmp);
+    
+		
+    // scaling
+    PolymerScaling::observableVSscalingSteps(
+	PolymerScaling::Observable::scalingParameter,
+	 polymer,
+	sp,
+	resultFile,
+	confFile,
+	numMonomersFile,
+	NULL
+    );
+    
+    delete [] resultFile;
+    
+    printf("Everything is OK!\n");
+    
+return 0;
+}
--- applications/various_k/main.cpp
+++ applications/various_k/main.cpp
@@ -8,16 +8,15 @@
 #include "PolymerObservable.h"
 #include "PolymerScaling.h"
 #include "DataHandler.h"
-//#include <string>
+#include "Logo.h"
 
+/* OPTIMIZATION NEEDED*/
 using namespace PCA;
 using namespace std;
 
 int main(int np, char **p)
 {	
-    double tmp;
-    char str [100];
-    bool currentVerbose;
+    Logo::print();
     
     if(p[1]==NULL){
 	printf("------------------------\n");
@@ -34,63 +33,57 @@ int main(int np, char **p)
 	exit(1);
     }
     
-    sprintf(str,"xyz_%s.dat",p[1]); //name of input file
+    printf("------------------------\n");
+    printf("I will calculate dependency of total angle on scaling step for\n");
+    printf("different number of 'k' (excluded from summation terms)\n");
+    printf("------------------------\n");
+    
+    printf("Polymer name: %s\n", p[1]);
+    globalVerbose = false;
+    
+    // create the full name of input file
+    char str [100];
+    sprintf(str,"xyz_%s.dat",p[1]);
     sprintf(str,"%s",DataHandler::fullName(str).c_str());
+    
+    //checking that all chains in the file have the same size, so can be taken for statistics
     _PCA_CHECK_BLOCKS_SIZE_IN_FILE(str);
+    printf("Number of blocks: %i\n",File::countBlocks(str));
+    printf("Number of sites in each block: %i\n",File::countLinesInBlock(str));
     
-    
+    // name of output file with configurations
     char confFile[100];
     sprintf(confFile,"results/%s_configurations.dat",p[1]);
     
+    // name of output file with number of monomers during scaling procedure
     char numMonomersFile[100];
     sprintf(numMonomersFile,"results/%s_numMonomers.dat",p[1]);
     
+    // name of output file with scaling parameter
     char scalingParamFile[100];
     sprintf(scalingParamFile,"results/%s_scalingParam.dat",p[1]);
     
     
-    globalVerbose = false;
-//    PolymerScaling::setVerbose(false);
-    char* resultFile;
-    Polymer* polymer;
     
-    printf("------------------------\n");
-    printf("I will calculate dependency of total angle on scaling step\n");
-    printf("------------------------\n");
-	
-	
+    Polymer* polymer;	
     polymer = new Polymer(str,0,1);
-    tmp = PolymerScaling::findCriticalScalingParam(polymer->getNumMonomers());
+    double tmp;
+    int N = polymer->getNumMonomers();
+    tmp = PolymerScaling::findCriticalScalingParam(N);
     PolymerScaling::ScalingParam sp(tmp);
+    
     delete polymer;
 	
-	
-	
-    for (int k=1;k<=2;k++){
+    char* resultFile;
+    
+    
+    for (int k=1;k<N-10;k++){
 	printf("k = %i\n",k);
 	resultFile = new char [100];
 	sprintf(resultFile,"results/%s_k%iVSsteps.dat",p[1],k); //name of output file with total angle
     
-//	    currentVerbose = globalVerbose;
-//	    globalVerbose = false;
-//	    polymer = new Polymer(str,0,1);
-//	    globalVerbose = currentVerbose;
-
-//	    FILE* lengthsFp;
-//	    lengthsFp = fopen("lengths.dat", "w");
-//    	    polymer->setMonomerLengthsFromRadiusVectors();
-//    	    polymer->writeMonomerLengthsInFile(lengthsFp);
-//    	    fclose(lengthsFp);
-        
-        
-    	//    printf("Radius of gyration: %g\n\n", PolymerObservable::radiusOfGyration(polymer));
-        
-	    
-
 	PolymerScaling::observableVSscalingStepsWithStatistics(
 	    PolymerScaling::Observable::totalAngle,
-//		PolymerScaling::Observable::test,
-//		PolymerScaling::Observable::averageMonomersLength,
 	    str,
 	    0,
 	    sp,
@@ -100,9 +93,7 @@ int main(int np, char **p)
 	    scalingParamFile,
 	    k
 	);
-//	    delete polymer;
 	delete [] resultFile;
-//	    Timer::tock(k);    
     }
     
     printf("Everything is OK!\n");
